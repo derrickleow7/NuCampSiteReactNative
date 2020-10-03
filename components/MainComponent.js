fb8064ffb9b24fe18ceb5e1ef69c0c7e30403f86
Derrick Leow <derrickleow7@gmail.com>
2020-10-03 13:00:33 -0700

    Assignment 4

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -319,19 +319,21 @@ const MainNavigator = createDrawerNavigator(
 
 class Main extends Component {
 
+    async showNetInfo() {
+    connectionInfo = await NetInfo.fetch();
+            (Platform.OS === 'ios') ?
+                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+        } 
+    
     componentDidMount() {
         this.props.fetchCampsites();
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
+        this.showNetInfo();
         console.log("test")
-
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios') ?
-                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
